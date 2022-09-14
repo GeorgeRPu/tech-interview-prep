927eaf9bfba97271b7baa24f1229cca7d5b43c57
George Pu <pu.george99@gmail.com>
2022-09-13 23:58:30 -0700

    Add memoization to ClimbingStairs

--- solutions/ClimbingStairs.py
+++ solutions/ClimbingStairs.py
@@ -19,11 +19,22 @@ Test
 3
 """
 
+climb_stairs = {}
 
-def climbStairs(n):
-    if n == 1:
-        return 1
+
+def climbStairs(n: int) -> int:
+    """Return the number of ways to climb ``n`` steps of stairs taking only
+    jumps of 1 and 2, using memoization to speed up the calculation.
+    """
+    if n > 2:
+        if n - 1 not in climb_stairs:
+            climb_stairs[n - 1] = climbStairs(n - 1)
+        x = climb_stairs[n - 1]
+        if n - 2 not in climb_stairs:
+            climb_stairs[n - 2] = climbStairs(n - 2)
+        y = climb_stairs[n - 2]
+        return x + y
     elif n == 2:
         return 2
     else:
-        return climbStairs(n - 1) + climbStairs(n - 2)
+        return 1
