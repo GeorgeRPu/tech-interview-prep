eef556528c23fd9d5d489494322aafb70f1e5b3d
George Pu <pu.george99@gmail.com>
2025-08-12 23:58:24 -0700

    Add Google Analytics

--- conf.py
+++ conf.py
@@ -40,6 +40,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
+    "sphinxcontrib.googleanalytics"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -62,3 +63,5 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+googleanalytics_id = "G-LE07FGQCP9"
